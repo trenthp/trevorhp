0b021b6d30560e9ae498c9a926a0bb9bfffef50b
Trent Petersen <Foxtrot@Foxtrot.local>
2014-03-23 17:27:31 -0700

    header with name
    added trevor’s own photos as well.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,9 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
-	$('.img-holder').imageScroll();
+	$('.img-holder').imageScroll({
+		speed: .3
+	});
 
 	console.log('Page Loaded. Lets Do this!');
 
